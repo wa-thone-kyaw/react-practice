f785ac2065dfc0aa270d5fa4df1d8a9261a81215
Wa Thone Kyaw <62678144+wa-thone-kyaw@users.noreply.github.com>
2024-07-22 06:43:29 +0630

    day strick

--- yaycha/src/App.jsx
+++ yaycha/src/App.jsx
@@ -1,25 +1,42 @@
 import { useState } from "react";
-import List from "./List.jsx";
-import Item from "./Item.jsx";
+
+import { Box, Container } from "@mui/material";
+
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Item from "./components/Item";
+
+import { useApp } from "./ThemedApp";
 
 export default function App() {
+  const { showForm } = useApp();
+
   const [data, setData] = useState([
-    { id: 1, content: "Hello, World!", name: "Alice" },
+    { id: 3, content: "Yay, interesting.", name: "Chris" },
     { id: 2, content: "React is fun.", name: "Bob" },
-    { id: 3, content: "Yay, interesting", name: "Chris" },
+    { id: 1, content: "Hello, World!", name: "Alice" },
   ]);
 
   const remove = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
+
+  const add = (content, name) => {
+    const id = data[0].id + 1;
+    setData([{ id, content, name }, ...data]);
+  };
+
   return (
-    <div style={{ maxWidth: 600, margin: "20px auto" }}>
-      <h1>Yaycha</h1>
-      <List>
+    <Box>
+      <Header />
+
+      <Container maxWidth="sm" sx={{ mt: 4 }}>
+        {showForm && <Form add={add} />}
+
         {data.map((item) => {
           return <Item key={item.id} item={item} remove={remove} />;
         })}
-      </List>
-    </div>
+      </Container>
+    </Box>
   );
 }
--- yaycha/src/main.jsx
+++ yaycha/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import ThemedApp from "./ThemedApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemedApp />
   </React.StrictMode>
 );
